6d27eb1894e2ef3c8d57499a4703b3f0ea66d727
igsem123 <136397682+igsem123@users.noreply.github.com>
2024-06-03 20:10:26 -0300

    versão 1.0

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,50 @@
+import { Image, StyleSheet, Platform } from 'react-native';
+
+import { HelloWave } from '@/components/HelloWave';
+import ParallaxScrollView from '@/components/ParallaxScrollView';
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+
+export default function HomeScreen() {
+  return (
+    <ParallaxScrollView
+      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
+      headerImage={
+        <Image
+          source={require('@/assets/images/partial-react-logo.png')}
+          style={styles.reactLogo}
+        />
+      }>
+      <ThemedView style={styles.titleContainer}>
+        <ThemedText type="title">Bem vindo!</ThemedText>
+        <HelloWave />
+      </ThemedView>
+      <ThemedText type="subtitle">Eu sou o Raphael!</ThemedText>
+      <ThemedView style={styles.stepContainer}>
+        <ThemedText type="subtitle">Etapa 1: concluída com sucesso!!</ThemedText>
+        <ThemedText>
+          Agora <ThemedText type="defaultSemiBold">vamos iniciar a segunda etapa e estilizar</ThemedText> o aplicativo.
+        </ThemedText>
+      </ThemedView>
+    </ParallaxScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  titleContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 8,
+  },
+  stepContainer: {
+    gap: 8,
+    marginBottom: 8,
+  },
+  reactLogo: {
+    height: 178,
+    width: 290,
+    bottom: 0,
+    left: 0,
+    position: 'absolute',
+  },
+});
